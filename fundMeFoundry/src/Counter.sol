b8ee0904dca0373d569b208cdc06d2ebea1544f9
n3rddev <n3rddev.code@gmail.com>
2026-01-30 18:26:08 +0530

    simple fund me contract

--- fundMeFoundry/src/Counter.sol
+++ fundMeFoundry/src/Counter.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.13;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    uint256 public constant minimumAmountInUSD = 5;
+    int256 public constant minimumAmountInUSD = 5;
     address immutable public owner;
-    mapping(string name => uint256 valueInUSD) fundings;
+    mapping(address wallet => int256 valueInUSD) fundings;
 
     AggregatorV3Interface public priceFeed;
 
@@ -16,6 +16,19 @@ contract FundMe {
     }
     
     function fund()public payable{
+        require(msg.value >= getPrice(), "at least 5$ needs to be funded");
+        fundings[msg.sender] += msg.value;
+    }
 
+    function getPrice()public view returns(uint256){
+        (,int256 answer,,,) = priceFeed.latestRoundData(); //3000$
+        int256 weiConversion = 5e18/answer;
+        return uint256(weiConversion);
     }
 }
+
+//Mathematics 
+// 1e18 = 1eth
+// 1eth = 3000$
+// 1e18 = 3000$
+// x wei = 5$
