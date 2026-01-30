84fe2800a0a524beee467547878017231f99d620
n3rddev <n3rddev.code@gmail.com>
2026-01-30 18:13:17 +0530

    variable initialisation contant clear

--- fundMeFoundry/src/Counter.sol
+++ fundMeFoundry/src/Counter.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    uint256 constant public minimumAmountInUSD = 5;
+    uint256 public constant minimumAmountInUSD = 5;
     address immutable public owner;
     mapping(string name => uint256 valueInUSD) fundings;
 
